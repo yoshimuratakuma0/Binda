7c170bf69ed1e9e8b1fe11e3e3849a833455433c
yoshimuratakuma0 <outofbody20@gmail.com>
2025-09-29 01:05:01 +0900

    Define CoroutineScreen

--- app/src/main/java/com/legstart/binda/MainActivity.kt
+++ app/src/main/java/com/legstart/binda/MainActivity.kt
@@ -4,25 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.legstart.binda.ui.theme.BindaTheme
 
 class MainActivity : ComponentActivity() {
-    private val viewModel by lazy {
+    private val coroutineViewModel by lazy {
         CoroutineViewModel(RxJava3FruitRepository())
     }
 
@@ -31,12 +33,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BindaTheme {
-                val fruits by viewModel.fruits.collectAsStateWithLifecycle()
-                val isLoading by viewModel.isLoading.collectAsStateWithLifecycle()
-                Screen(
-                    fruits = fruits,
-                    isLoading = isLoading,
-                    modifier = Modifier.fillMaxSize(),
+                val fruitsByCoroutine by coroutineViewModel.fruits.collectAsStateWithLifecycle()
+                val isLoadingByCoroutine by coroutineViewModel.isLoading.collectAsStateWithLifecycle()
+                MainScreen(
+                    isLoadingByCoroutine = isLoadingByCoroutine,
+                    fruitsByCoroutine = fruitsByCoroutine,
                 )
             }
         }
@@ -44,29 +45,66 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Screen(
-    fruits: List<String>,
-    isLoading: Boolean,
-    modifier: Modifier = Modifier,
+fun MainScreen(
+    isLoadingByCoroutine: Boolean,
+    fruitsByCoroutine: List<String>,
 ) {
-    Scaffold(modifier = modifier.fillMaxSize()) { innerPadding ->
-        if (isLoading) {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(color = Color(0x80000000)),
-                contentAlignment = Alignment.Center,
-            ) {
-                CircularProgressIndicator(modifier = Modifier)
-            }
-        }
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         LazyColumn(
+            modifier = Modifier
+                .fillMaxSize(),
             contentPadding = innerPadding,
         ) {
-            items(fruits) { fruit ->
-                Text(text = fruit)
+            item {
+                Text(text = "Fruits from Coroutine")
+            }
+            item {
+                FruitsScreen(
+                    isLoading = isLoadingByCoroutine,
+                    fruits = fruitsByCoroutine,
+                    modifier = Modifier.fillMaxWidth(),
+                )
+            }
+            item {
+                HorizontalDivider()
             }
         }
+    }
+}
+
+@Composable
+fun FruitsScreen(
+    isLoading: Boolean,
+    fruits: List<String>,
+    modifier: Modifier = Modifier,
+) {
+    if (isLoading) {
+        Box(
+            modifier = modifier
+                .height(100.dp)
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center,
+        ) {
+            CircularProgressIndicator(modifier = Modifier)
+        }
+    }
+    Column(
+        modifier = modifier,
+    ) {
+        fruits.forEach {
+            Text(text = it)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainScreenPreview() {
+    BindaTheme {
+        MainScreen(
+            isLoadingByCoroutine = false,
+            fruitsByCoroutine = listOf("banana", "apple", "orange"),
+        )
     }
 }
 
@@ -74,10 +112,9 @@ fun Screen(
 @Composable
 private fun LoadingPreview() {
     BindaTheme {
-        Screen(
-            fruits = listOf("banana", "apple", "orange"),
+        FruitsScreen(
+            fruits = emptyList(),
             isLoading = true,
-            modifier = Modifier.fillMaxSize(),
         )
     }
 }
