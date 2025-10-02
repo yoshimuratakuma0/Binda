2400f65b187755158e5933cc26195d8323101eab
yoshimuratakuma0 <outofbody20@gmail.com>
2025-10-03 00:53:14 +0900

    Publish it as library

--- core-async/build.gradle.kts
+++ core-async/build.gradle.kts
@@ -1,17 +1,11 @@
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
+    id("signing")
+    id("com.vanniktech.maven.publish") version "0.34.0"
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-    }
-}
+kotlin { compilerOptions { jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11 } }
 
 dependencies {
     implementation(project(":core"))
@@ -20,4 +14,35 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.mockk)
     testImplementation(libs.kotlinx.coroutines.test)
+}
+
+val artifactVersion = "1.0.0"
+mavenPublishing {
+    coordinates("com.legstart", "binda-core-async", artifactVersion)
+
+    pom {
+        name.set("Binda Core Async")
+        description.set("Core abstractions for async task binding")
+        inceptionYear.set("2025")
+        url.set("https://github.com/yoshimuratakuma0/Binda")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0");
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt");
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("yoshimuratakuma0");
+                name.set("Takuma Yoshimura");
+                url.set("https://github.com/yoshimuratakuma0/")
+            }
+        }
+        scm {
+            url.set("https://github.com/yoshimuratakuma0/Binda");
+            connection.set("scm:git:git://github.com/yoshimuratakuma0/Binda.git");
+            developerConnection.set("scm:git:ssh://git@github.com/yoshimuratakuma0/Binda.git")
+        }
+    }
 }
